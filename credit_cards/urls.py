7a8bc71723259043be29b595ec652df7397e45af
Alexvargame <104843247+Alexvargame@users.noreply.github.com>
2023-01-07 12:21:00 +0200

    Update urls.py

--- credit_cards/urls.py
+++ credit_cards/urls.py
@@ -20,4 +20,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('cards.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
